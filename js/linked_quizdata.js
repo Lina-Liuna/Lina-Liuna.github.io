18e2944d77850006b750e0b2642f696d0c5a4a41
Lina <liuna.lina@gmail.com>
2023-08-02 11:13:29 -0700

    update accessment part

---
+++ js/linked_quizdata.js
@@ -0,0 +1,11 @@
+// quizData.js
+const quiz = [
+  {
+    question: "What cmd let you create a connection between a local and remote repository?",
+    options: ["git remote add new", "git remote add origin", "git remote new origin", "git remote origin"],
+    answer: "git remote add origin"
+  },
+  // Add more quiz questions and answers here
+];
+
+export default quiz;
---
+++ js/linkedin_quiz.js
@@ -0,0 +1,66 @@
+import quiz from './linked_quizdata.js'; // Replace the correct path to quizData.js
+
+// Rest of your quiz logic here...
+
+
+const questionElement = document.getElementById("question");
+const optionsElement = document.getElementById("options");
+const submitButton = document.getElementById("submitBtn");
+const resultElement = document.getElementById("result");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function displayQuestion() {
+  const currentQuestion = quiz[currentQuestionIndex];
+  questionElement.textContent = currentQuestion.question;
+  optionsElement.innerHTML = "";
+
+  currentQuestion.options.forEach((option) => {
+    const input = document.createElement("input");
+    input.type = "radio";
+    input.name = "option";
+    input.value = option;
+    optionsElement.appendChild(input);
+
+    const label = document.createElement("label");
+    label.textContent = option;
+    optionsElement.appendChild(label);
+
+    optionsElement.appendChild(document.createElement("br"));
+  });
+}
+
+function checkAnswer() {
+  const selectedOption = document.querySelector('input[name="option"]:checked');
+
+  if (!selectedOption) {
+    alert("Please select an option.");
+    return;
+  }
+
+  const answer = selectedOption.value;
+  const currentQuestion = quiz[currentQuestionIndex];
+  if (answer === currentQuestion.answer) {
+    score++;
+  }
+
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex < quiz.length) {
+    displayQuestion();
+  } else {
+    displayResult();
+  }
+}
+
+function displayResult() {
+  quiz.innerHTML = "";
+  resultElement.style.display = "block";
+  resultElement.textContent = `You scored ${score} out of ${quiz.length}.`;
+}
+
+submitButton.addEventListener("click", checkAnswer);
+
+// Display the first question when the page loads
+displayQuestion();
