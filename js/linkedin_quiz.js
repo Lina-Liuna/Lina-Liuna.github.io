8ac249cf558c1fb6917cc5d4d5ab751d5d40ce49
Lina <liuna.lina@gmail.com>
2023-08-02 13:41:07 -0700

    only displayone question

--- js/linkedin_quiz.js
+++ js/linkedin_quiz.js
@@ -1,22 +1,25 @@
 import quiz from './linked_quizdata.js'; // Replace the correct path to quizData.js
 
-// Rest of your quiz logic here...
-
-
-const questionElement = document.getElementById("question");
-const optionsElement = document.getElementById("options");
-const submitButton = document.getElementById("submitBtn");
-const resultElement = document.getElementById("result");
-
 let currentQuestionIndex = 0;
-let score = 0;
 
+// Function to shuffle an array using the Fisher-Yates algorithm
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+// Function to display the current question
 function displayQuestion() {
   const currentQuestion = quiz[currentQuestionIndex];
+  const questionElement = document.getElementById("question");
+  const optionsElement = document.getElementById("options");
+
   questionElement.textContent = currentQuestion.question;
   optionsElement.innerHTML = "";
 
-  currentQuestion.options.forEach((option) => {
+  currentQuestion.options.forEach((option, index) => {
     const input = document.createElement("input");
     input.type = "radio";
     input.name = "option";
@@ -31,6 +34,7 @@ function displayQuestion() {
   });
 }
 
+// Function to check the answer and move to the next question
 function checkAnswer() {
   const selectedOption = document.querySelector('input[name="option"]:checked');
 
@@ -42,7 +46,7 @@ function checkAnswer() {
   const answer = selectedOption.value;
   const currentQuestion = quiz[currentQuestionIndex];
   if (answer === currentQuestion.answer) {
-    score++;
+    // Handle the correct answer here (e.g., increment score)
   }
 
   currentQuestionIndex++;
@@ -54,15 +58,19 @@ function checkAnswer() {
   }
 }
 
+// Function to display the final result
 function displayResult() {
-  quiz.innerHTML = "";
-  resultElement.style.display = "block";
-  resultElement.textContent = `You scored ${score} out of ${quiz.length}.`;
+  const resultElement = document.getElementById("result");
+  const score = 0; // Calculate the score based on the correct answers
+  resultElement.textContent = `Your score: ${score} out of ${quiz.length}`;
 }
 
+// Shuffle the quiz array before displaying the questions
+shuffleArray(quiz);
+
+// Get the submit button and attach the checkAnswer function to it
+const submitButton = document.getElementById("submitBtn");
 submitButton.addEventListener("click", checkAnswer);
 
 // Display the first question when the page loads
 displayQuestion();
-checkAnswer();
-displayResult();
